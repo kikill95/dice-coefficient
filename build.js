01f2442d676a828d4a0e5db81cd6b54d229232e6
Titus Wormer <tituswormer@gmail.com>
2016-11-05 14:38:42 +0100

    Update

--- build.js
+++ build.js
@@ -1,195 +1,82 @@
-(function outer(modules, cache, entries){
-
-  /**
-   * Global
-   */
-
-  var global = (function(){ return this; })();
-
-  /**
-   * Require `name`.
-   *
-   * @param {String} name
-   * @param {Boolean} jumped
-   * @api public
-   */
-
-  function require(name, jumped){
-    if (cache[name]) return cache[name].exports;
-    if (modules[name]) return call(name, require);
-    throw new Error('cannot find module "' + name + '"');
-  }
-
-  /**
-   * Call module `id` and cache it.
-   *
-   * @param {Number} id
-   * @param {Function} require
-   * @return {Function}
-   * @api private
-   */
-
-  function call(id, require){
-    var m = cache[id] = { exports: {} };
-    var mod = modules[id];
-    var name = mod[2];
-    var fn = mod[0];
-
-    fn.call(m.exports, function(req){
-      var dep = modules[id][1][req];
-      return require(dep ? dep : req);
-    }, m, m.exports, outer, modules, cache, entries);
-
-    // expose as `name`.
-    if (name) cache[name] = cache[id];
-
-    return cache[id].exports;
-  }
-
-  /**
-   * Require all entries exposing them on global if needed.
-   */
-
-  for (var id in entries) {
-    if (entries[id]) {
-      global[entries[id]] = require(id);
-    } else {
-      require(id);
-    }
-  }
-
-  /**
-   * Duo flag.
-   */
-
-  require.duo = true;
-
-  /**
-   * Expose cache.
-   */
-
-  require.cache = cache;
-
-  /**
-   * Expose modules
-   */
-
-  require.modules = modules;
-
-  /**
-   * Return newest require.
-   */
-
-   return require;
-})({
-1: [function(require, module, exports) {
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-/**
- * Dependencies.
- */
+/* Dependencies. */
+var diceCoefficient = require('dice-coefficient');
 
-var diceCoefficient = require('wooorm/dice-coefficient@0.1.3');
-
-/**
- * DOM elements.
- */
-
+/* Nodes. */
 var $input = document.getElementsByTagName('input')[0];
 var $reference = document.getElementsByTagName('input')[1];
 var $output = document.getElementsByTagName('output')[0];
 
-/**
- * Event handlers.
- */
-
-function oninputchange() {
-    $output.textContent = diceCoefficient($input.value, $reference.value);
-}
-
-/**
- * Listen.
- */
-
+/* Listen. */
 $input.addEventListener('input', oninputchange);
 $reference.addEventListener('input', oninputchange);
 
-/**
- * Initial answer.
- */
-
+/* Initial answer. */
 oninputchange();
 
-}, {"wooorm/dice-coefficient@0.1.3":2}],
-2: [function(require, module, exports) {
-'use strict';
+/* Calc. */
+function oninputchange() {
+  $output.textContent = diceCoefficient($input.value, $reference.value);
+}
 
-var getBigrams;
-
+},{"dice-coefficient":2}],2:[function(require,module,exports){
 /**
- * Module dependencies.
+ * @author Titus Wormer
+ * @copyright 2014 Titus Wormer
+ * @license MIT
+ * @module dice-coefficient
+ * @fileoverview Sørensen–Dice coefficient.
  */
 
-getBigrams = require('n-gram').bigram;
+'use strict';
+
+/* Dependencies. */
+var bigrams = require('n-gram').bigram;
+
+/* Expose. */
+module.exports = diceCoefficient;
 
 /**
  * Get the edit-distance according to Dice between two values.
  *
  * @param {*} value - First value.
- * @param {*} alternative - Second value.
+ * @param {*} right - Second value.
  * @return {number} Edit distance.
  */
-
 function diceCoefficient(value, alternative) {
-    var pairs,
-        alternativePairs,
-        intersections,
-        iterator,
-        length,
-        alternativeLength,
-        alternativeIterator,
-        alternativePair,
-        pair;
+  var left = bigrams(String(value).toLowerCase());
+  var right = bigrams(String(alternative).toLowerCase());
+  var rightLength = right.length;
+  var length = left.length;
+  var index = -1;
+  var intersections = 0;
+  var rightPair;
+  var leftPair;
+  var offset;
 
-    pairs = getBigrams(String(value).toLowerCase());
-    alternativePairs = getBigrams(String(alternative).toLowerCase());
-    intersections = 0;
-    iterator = -1;
-    alternativeLength = alternativePairs.length;
-    length = pairs.length;
+  while (++index < length) {
+    leftPair = left[index];
+    offset = -1;
 
-    while (++iterator < length) {
-        pair = pairs[iterator];
+    while (++offset < rightLength) {
+      rightPair = right[offset];
 
-        alternativeIterator = -1;
+      if (leftPair === rightPair) {
+        intersections++;
 
-        while (++alternativeIterator < alternativeLength) {
-            alternativePair = alternativePairs[alternativeIterator];
-
-            if (pair === alternativePair) {
-                intersections++;
-
-                /**
-                 * Make sure this pair never matches again
-                 */
-
-                alternativePairs[alternativeIterator] = '';
-                break;
-            }
-        }
+        /* Make sure this pair never matches again */
+        right[offset] = '';
+        break;
+      }
     }
+  }
 
-    return 2 * intersections / (pairs.length + alternativeLength);
+  return 2 * intersections / (left.length + rightLength);
 }
 
-/**
- * Expose `diceCoefficient`.
- */
-
-module.exports = diceCoefficient;
-
-}, {"n-gram":3}],
-3: [function(require, module, exports) {
+},{"n-gram":3}],3:[function(require,module,exports){
 'use strict';
 
 /**
@@ -206,7 +93,6 @@ module.exports = diceCoefficient;
  * @throws {Error} When `n` is not a number (incl. NaN), Infinity, or lt 1.
  * @return {Function} A function creating n-grams from a given value.
  */
-
 function nGram(n) {
     if (
         typeof n !== 'number' ||
@@ -219,7 +105,7 @@ function nGram(n) {
         );
     }
 
-    /**
+    /*
      * Create n-grams from a given value.
      *
      * @example
@@ -231,18 +117,21 @@ function nGram(n) {
      */
 
     return function (value) {
-        var nGrams = [],
+        var nGrams,
             index;
+
+        nGrams = [];
 
         if (value === null || value === undefined) {
             return nGrams;
         }
 
         value = String(value);
+
         index = value.length - n + 1;
 
         if (index < 1) {
-            return [];
+            return nGrams;
         }
 
         while (index--) {
@@ -253,13 +142,13 @@ function nGram(n) {
     };
 }
 
-/**
+/*
  * Export `n-gram`.
  */
 
 module.exports = nGram;
 
-/**
+/*
  * Create bigrams from a given value.
  *
  * @example
@@ -272,7 +161,7 @@ module.exports = nGram;
 
 nGram.bigram = nGram(2);
 
-/**
+/*
  * Create trigrams from a given value.
  *
  * @example
@@ -285,4 +174,4 @@ nGram.bigram = nGram(2);
 
 nGram.trigram = nGram(3);
 
-}, {}]}, {}, {"1":""})
+},{}]},{},[1]);
--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 'use strict';
 
-/**
- * Dependencies.
- */
+/* Dependencies. */
+var diceCoefficient = require('dice-coefficient');
 
-var diceCoefficient = require('wooorm/dice-coefficient@0.1.3');
-
-/**
- * DOM elements.
- */
-
+/* Nodes. */
 var $input = document.getElementsByTagName('input')[0];
 var $reference = document.getElementsByTagName('input')[1];
 var $output = document.getElementsByTagName('output')[0];
 
-/**
- * Event handlers.
- */
-
-function oninputchange() {
-    $output.textContent = diceCoefficient($input.value, $reference.value);
-}
-
-/**
- * Listen.
- */
-
+/* Listen. */
 $input.addEventListener('input', oninputchange);
 $reference.addEventListener('input', oninputchange);
 
-/**
- * Initial answer.
- */
-
+/* Initial answer. */
 oninputchange();
+
+/* Calc. */
+function oninputchange() {
+  $output.textContent = diceCoefficient($input.value, $reference.value);
+}
