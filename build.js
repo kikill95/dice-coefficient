967c51dab54028b6d162a84cc7d57990864d8783
Titus Wormer <tituswormer@gmail.com>
2017-07-09 10:23:04 +0200

    Rebuild

--- build.js
+++ build.js
@@ -1,50 +1,29 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 
-/* Dependencies. */
 var diceCoefficient = require('dice-coefficient');
 
-/* Nodes. */
 var $input = document.getElementsByTagName('input')[0];
 var $reference = document.getElementsByTagName('input')[1];
 var $output = document.getElementsByTagName('output')[0];
 
-/* Listen. */
 $input.addEventListener('input', oninputchange);
 $reference.addEventListener('input', oninputchange);
 
-/* Initial answer. */
 oninputchange();
 
-/* Calc. */
 function oninputchange() {
   $output.textContent = diceCoefficient($input.value, $reference.value);
 }
 
 },{"dice-coefficient":2}],2:[function(require,module,exports){
-/**
- * @author Titus Wormer
- * @copyright 2014 Titus Wormer
- * @license MIT
- * @module dice-coefficient
- * @fileoverview Sørensen–Dice coefficient.
- */
-
 'use strict';
 
-/* Dependencies. */
 var bigrams = require('n-gram').bigram;
 
-/* Expose. */
 module.exports = diceCoefficient;
 
-/**
- * Get the edit-distance according to Dice between two values.
- *
- * @param {*} value - First value.
- * @param {*} right - Second value.
- * @return {number} Edit distance.
- */
+/* Get the edit-distance according to Dice between two values. */
 function diceCoefficient(value, alternative) {
   var left = bigrams(String(value).toLowerCase());
   var right = bigrams(String(alternative).toLowerCase());
@@ -79,99 +58,43 @@ function diceCoefficient(value, alternative) {
 },{"n-gram":3}],3:[function(require,module,exports){
 'use strict';
 
-/**
- * A factory returning a function that converts a given string to n-grams.
- *
- * @example
- *   nGram(2) // [Function]
- *
- * @example
- *   nGram(4) // [Function]
- *
- *
- * @param {number} n - The `n` in n-gram.
- * @throws {Error} When `n` is not a number (incl. NaN), Infinity, or lt 1.
- * @return {Function} A function creating n-grams from a given value.
- */
+/* Expose. */
+module.exports = exports = nGram;
+
+exports.bigram = nGram(2);
+exports.trigram = nGram(3);
+
+/* Factory returning a function that converts a given string
+ * to n-grams. */
 function nGram(n) {
-    if (
-        typeof n !== 'number' ||
-        n < 1 ||
-        n !== n ||
-        n === Infinity
-    ) {
-        throw new Error(
-            'Type error: `' + n + '` is not a valid argument for n-gram'
-        );
+  if (typeof n !== 'number' || isNaN(n) || n < 1 || n === Infinity) {
+    throw new Error('`' + n + '` is not a valid argument for n-gram');
+  }
+
+  return grams;
+
+  /* Create n-grams from a given value. */
+  function grams(value) {
+    var nGrams = [];
+    var index;
+
+    if (value === null || value === undefined) {
+      return nGrams;
     }
 
-    /*
-     * Create n-grams from a given value.
-     *
-     * @example
-     *   nGram(4)('n-gram')
-     *   // ['n-gr', '-gra', 'gram']
-     *
-     * @param {*} value - The value to stringify and convert into n-grams.
-     * @return {Array.<string>} n-grams
-     */
+    value = String(value);
+    index = value.length - n + 1;
 
-    return function (value) {
-        var nGrams,
-            index;
+    if (index < 1) {
+      return nGrams;
+    }
 
-        nGrams = [];
+    while (index--) {
+      nGrams[index] = value.substr(index, n);
+    }
 
-        if (value === null || value === undefined) {
-            return nGrams;
-        }
-
-        value = String(value);
-
-        index = value.length - n + 1;
-
-        if (index < 1) {
-            return nGrams;
-        }
-
-        while (index--) {
-            nGrams[index] = value.substr(index, n);
-        }
-
-        return nGrams;
-    };
+    return nGrams;
+  }
 }
-
-/*
- * Export `n-gram`.
- */
-
-module.exports = nGram;
-
-/*
- * Create bigrams from a given value.
- *
- * @example
- *   bigram('n-gram')
- *   // ["n-", "-g", "gr", "ra", "am"]
- *
- * @param {*} value - The value to stringify and convert into bigrams.
- * @return {Array.<string>} bigrams
- */
-
-nGram.bigram = nGram(2);
-
-/*
- * Create trigrams from a given value.
- *
- * @example
- *   trigram('n-gram')
- *   // ["n-g", "-gr", "gra", "ram"]
- *
- * @param {*} value - The value to stringify and convert into trigrams.
- * @return {Array.<string>} trigrams
- */
-
-nGram.trigram = nGram(3);
 
 },{}]},{},[1]);
--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 'use strict';
 
-/* Dependencies. */
 var diceCoefficient = require('dice-coefficient');
 
-/* Nodes. */
 var $input = document.getElementsByTagName('input')[0];
 var $reference = document.getElementsByTagName('input')[1];
 var $output = document.getElementsByTagName('output')[0];
 
-/* Listen. */
 $input.addEventListener('input', oninputchange);
 $reference.addEventListener('input', oninputchange);
 
-/* Initial answer. */
 oninputchange();
 
-/* Calc. */
 function oninputchange() {
   $output.textContent = diceCoefficient($input.value, $reference.value);
 }
